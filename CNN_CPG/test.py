e0c7b206c53d5d68fd39951b6a30c9f1f9930d86
Yuecai Zhu <yzhu@cortex.meaney.lab>
2018-06-03 14:11:28 -0400

    finalize structure

--- CNN_CPG/test.py
+++ CNN_CPG/test.py
@@ -7,8 +7,8 @@ from keras.models import Sequential
 from imblearn.over_sampling import SMOTE 
 from keras import optimizers
 
-from numpy.random import seed
-seed(1)
+#from numpy.random import seed
+#seed(2)
 
 """
 without bias, 0.667 accuracy
@@ -23,21 +23,23 @@ def connect_to_db(dbname):
 
 
 
-num_cpg = 1000
+num_cpg = 2000
 num_kids=176
-nb_filter=50
+nb_filter1=200
+nb_filter2=20
 nb_class = 2
-filter_length = 2
+filter_length1 = 3
+filter_length2 = 3
 with_bias=False
-n_epoch = 300
+n_epoch = 100
 batch_size = 1
 save_path ="trained_model/my_model%s.h5"
-dropout = 0.25
+dropout = 0.1
 
-query_get_cpg = "select cpg,beta,study_id,chr,mapinfo from beta_value a,cpg_sites b where cpg = ilmnid and cpg in (select cpg from cg_p_dx limit %s) and study_id in (select study_id from kids) order by study_id,chr,mapinfo" 
+query_get_cpg = "select cpg,beta,study_id,chr,mapinfo from beta_value a,cpg_sites b where cpg = ilmnid and cpg in (select cpg from cg_p_abuse_bh where abuse_p < 0.05 limit %s) and study_id in (select study_id from kids) order by study_id,chr,mapinfo" 
 
 query_get_pheno= "select study_id,abuse,not_abuse from abuse_or_not order by study_id" 
-
+#query_get_pheno= "select study_id,childsex-1 from nfp_covariates order by study_id" 
 def leave_one_cnn(cpgs,phenos,which_leave):
     correct =0
     X = []
@@ -101,13 +103,23 @@ def leave_one_cnn(cpgs,phenos,which_leave):
     #X_res, Y_res = sm.fit_sample(X_train, Y_train)
 
     X_res, Y_res = sm.fit_sample(X, Y)
-
+    """
+    X_train, X_val, Y_train, Y_val = train_test_split(X_res,Y_res,test_size=0.05, random_state=42)
     #X=np.transpose(X)
-    X_res = np.expand_dims(X_res, axis=2) 
-
+    X_train = np.expand_dims(X_train, axis=2) 
+    #X_train = np.expand_dims(X_train, axis=1) 
+    X_val= np.expand_dims(X_val, axis=2) 
+    """
+    print(X.shape)
+    print(X_res.shape)
+    X_train = np.expand_dims(X_res, axis=2) 
+    X_val = X_train
     X_test = np.expand_dims(X_test, axis=1) 
     X_test = np.expand_dims(X_test, axis=0) 
-    Y_res=np.column_stack((Y_res,1-Y_res))
+    #Y_train=np.column_stack((Y_train,1-Y_train))
+    Y_train=np.column_stack((Y_res,1-Y_res))
+    #Y_val=np.column_stack((Y_val,1-Y_val))
+    Y_val = Y_train   
     Y_test = np.column_stack((Y_test,1-Y_test))
     #Y_res = np.expand_dims(Y_res, axis=1)
     #np.column_stack((Y_res,1-Y_res))
@@ -119,31 +131,46 @@ def leave_one_cnn(cpgs,phenos,which_leave):
     #print(X_test.shape)
     #print(Y_test.shape)
 
+#deep feed forward
+    """
+    model = Sequential((
+        Dense(1000,input_dim = num_cpg,activation='linear',use_bias=with_bias),
+        Dropout(dropout),
+        Dense(500,activation='linear',use_bias=with_bias),
+        Dropout(dropout),
+        Dense(300,activation='linear',use_bias=with_bias),
+        Dropout(dropout),
+        Dense(nb_class, activation='sigmoid'),
+        ))
+
+    #deep cnn
+    """
     model = Sequential((
         # The first conv layer learns `nb_filter` filters (aka kernels), each of size ``(filter_length, nb_input_series)``.
         # Its output will have shape (None, window_size - filter_length + 1, nb_filter), i.e., for each position in
         # the input timeseries, the activation of each filter at that position.
-        Convolution1D(nb_filter=nb_filter, filter_length=filter_length, activation='relu', input_shape=(num_cpg,1),use_bias=with_bias),
+        Convolution1D(nb_filter=nb_filter1, filter_length=filter_length1, activation='relu', input_shape=(num_cpg,1),use_bias=with_bias),
         MaxPooling1D(),     # Downsample the output of convolution by 2X.
         Dropout(dropout),
-        Convolution1D(nb_filter=nb_filter, filter_length=filter_length, activation='relu',use_bias=with_bias),
+        Convolution1D(nb_filter=nb_filter2, filter_length=filter_length2, activation='relu',use_bias=with_bias),
         MaxPooling1D(),
         Dropout(dropout),
         Flatten(),
+        Dense(nb_filter2, activation='sigmoid'),     # For binary classification, change the activation to 'sigmoid'
         Dense(nb_class, activation='sigmoid'),     # For binary classification, change the activation to 'sigmoid'
         Activation('softmax')
         ))
-    adam=optimizers.Adam( amsgrad=True)
-    #sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+     
+    adam=optimizers.Adam()
+    sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
     # To perform (binary) classification instead:
     # sgd is reported for better val accuracy: https://arxiv.org/abs/1705.08292
-    #model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['binary_accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
 
-    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['binary_accuracy'])
-    print('\n\nModel with input size {}, output size {}, {} conv filters of length {}'.format(model.input_shape,
-        model.output_shape, nb_filter, filter_length))
+    #model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['binary_accuracy'])
+    #print('\n\nModel with input size {}, output size {}, {} conv filters of length {}'.format(model.input_shape,model.output_shape, nb_filter1, filter_length1))
     model.summary()
-    model.fit(X_res, Y_res, epochs=n_epoch, batch_size=batch_size, shuffle=True,validation_data=(X_res, Y_res))
+    model.fit(X_train, Y_train, epochs=n_epoch, batch_size=batch_size, shuffle=True,validation_data=(X_val, Y_val))
     pred = model.predict(X_test)
     #print(pred[0])
     #print(Y_test[0])
@@ -172,11 +199,11 @@ if __name__=="__main__":
 
     conn.execute(query_get_pheno)
     phenos = conn.fetchall()
-    #predictions = [leave_one_cnn(cpgs,phenos,i) for i in range(2)]
-    predictions = [leave_one_cnn(cpgs,phenos,i) for i in range(num_kids)]
+    predictions = [leave_one_cnn(cpgs,phenos,i) for i in range(4)]
+    #predictions = [leave_one_cnn(cpgs,phenos,i) for i in range(num_kids)]
     n_correct = sum([p[2] for p in predictions])
-    precision = float(n_correct)/num_kids
-    #precision = float(n_correct)/2
+    #precision = float(n_correct)/num_kids
+    precision = float(n_correct)/4
     n_abuse_correct = sum([p[2] for p in predictions if p[1]==1])
     n_abuse = sum([p[1] for p in predictions])
     true_positive_rate= float(n_abuse_correct)/float(n_abuse)
